189e2f8f5136a43f6066200f648171c7cc18b0ba
AE <annisgrey@gmail.com>
2026-06-18 18:28:15 -0400

    resolving type issue and updating links

--- types.d.ts
+++ types.d.ts
@@ -1,11 +1,1 @@
-
-declare module '@joshtwc/react-places-autocomplete' {
-  export {
-    geocodeByAddress,
-    geocodeByPlaceId,
-    getLatLng
-  } from 'react-places-autocomplete';
-  export function geocodeByCoordinates(lat: number, lng: number): Promise<google.maps.GeocoderResult[]>;
-
-  export default function PlacesAutocomplete(props: any): JSX.Element;
-}
+declare module '@aegrey/react-places-autocomplete';
